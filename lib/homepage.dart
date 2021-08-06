88032f859d67437d42765eba1bd476d129bfe970
Maulik Shah <maulik.shah715@gmail.com>
2021-08-06 13:41:14 +0530

    Now data is fetched from API instead of dummy data

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,6 +1,7 @@
+import 'dart:convert';
 import 'package:covid_tracker/panels/worldwide.dart';
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart' as http;
 import 'datasource.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,15 +11,27 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-// Right now this is static data but it will be fetched from API
-Map worldData = {
-  "cases": 123456,
-  "recovered": 123456,
-  "active": 123456,
-  "deaths": 123456,
-};
-
 class _HomePageState extends State<HomePage> {
+  Map worldData = {
+    "cases": "Loading",
+    "deaths": "Loading",
+    "active": "Loading",
+    "recovered": "Loading",
+  };
+  fetchWorldData() async {
+    http.Response response =
+        await http.get(Uri.parse("https://disease.sh/v3/covid-19/all"));
+    setState(() {
+      worldData = jsonDecode(response.body);
+    });
+  }
+
+  @override
+  void initState() {
+    fetchWorldData();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
